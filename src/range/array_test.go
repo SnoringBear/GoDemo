ee8e9468768c8c04f2dd65c8663e198bf26ff414
andy <andy@gmail.com>
2024-08-08 21:53:34 +0800

    add

---
+++ src/range/array_test.go
@@ -0,0 +1,13 @@
+package _range
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test01(t *testing.T) {
+	a := []int{1, 2, 3, 4, 5}
+	for index, item := range a {
+		fmt.Printf("index = %d, item = %d\n", index, item)
+	}
+}
