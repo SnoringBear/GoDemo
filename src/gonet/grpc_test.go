5d305b395df5853092c55a7ea5ea0132c583fc74
anye <anye@gmail.com>
2024-08-23 14:13:29 +0800

    add

--- src/gonet/grpc_test.go
+++ src/gonet/grpc_test.go
@@ -5,6 +5,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"log"
+	"net"
 	"testing"
 )
 
@@ -24,4 +25,24 @@ func TestGrpc02(t *testing.T) {
 	// 启用 TLS 的 gRPC 服务器
 	grpcServer := grpc.NewServer(grpc.Creds(creeds))
 	fmt.Println(grpcServer)
+}
+
+func TestGrpc03(t *testing.T) {
+	// 创建 gRPC 服务器
+	grpcServer := grpc.NewServer()
+
+	// 注册服务
+	// pb.RegisterMyServiceServer(grpcServer, &myService{})
+
+	// 启动监听器
+	fmt.Println("1111111111111111111111111111111111")
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	fmt.Println("22222222222222222222222222222222222")
+	// 启动 gRPC 服务器
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
