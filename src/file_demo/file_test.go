9e198e2a585168230481f6c18e73e68365404c37
anye <hnkjxy2015@outlook.com>
2025-03-02 21:25:37 +0800

    file

--- src/file_demo/file_test.go
+++ src/file_demo/file_test.go
@@ -1,1 +1,18 @@
 package file_demo
+
+import (
+	"github.com/rs/zerolog/log"
+	"os"
+	"testing"
+)
+
+func TestFileExist01(t *testing.T) {
+	dir, _ := os.Getwd()
+	log.Info().Msgf("current dir:%s", dir) // reslut:D:\\GoDemo\\src\\file_demo
+	fileInfo, err := os.Stat(dir + "/file.md")
+	if err != nil {
+		log.Error().Msgf("err:%v", err)
+		return
+	}
+	log.Info().Msgf("fileInfo:%v", fileInfo)
+}
