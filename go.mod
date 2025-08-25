5f9bed3ca3252a40559d1ff546846936a91bdd82
anye <anye@gmail.com>
2025-08-25 21:32:00 +0800

    asnyc

--- go.mod
+++ go.mod
@@ -40,6 +40,7 @@ require (
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/go-querystring v1.0.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
@@ -53,6 +54,7 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/mozillazg/go-httpheader v0.2.1 // indirect
 	github.com/nats-io/nats.go v1.42.0 // indirect
 	github.com/nats-io/nkeys v0.4.11 // indirect
@@ -72,14 +74,20 @@ require (
 	github.com/tencentyun/cos-go-sdk-v5 v0.7.67 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
 	go.etcd.io/etcd/api/v3 v3.6.0 // indirect
 	go.etcd.io/etcd/client/pkg/v3 v3.6.0 // indirect
 	go.etcd.io/etcd/client/v3 v3.6.0 // indirect
+	go.mongodb.org/mongo-driver v1.17.4 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/arch v0.8.0 // indirect
 	golang.org/x/crypto v0.37.0 // indirect
 	golang.org/x/net v0.38.0 // indirect
+	golang.org/x/sync v0.16.0 // indirect
 	golang.org/x/sys v0.34.0 // indirect
 	golang.org/x/text v0.27.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20250303144028-a0af3efb3deb // indirect
---
+++ src/mongo_demo/mongo_test.go
@@ -0,0 +1,217 @@
+package mongo_demo
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"testing"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// 定义一个示例结构体
+type User struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	Name     string             `bson:"name"`
+	Age      int                `bson:"age"`
+	Email    string             `bson:"email"`
+	CreateAt time.Time          `bson:"createAt"`
+}
+
+func TestMongo01(t *testing.T) {
+	// 1. 连接MongoDB
+	client, err := connectMongoDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(client *mongo.Client, ctx context.Context) {
+		err := client.Disconnect(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(client, context.TODO())
+
+	// 获取数据库和集合
+	db := client.Database("testdb")
+	collection := db.Collection("users")
+
+	// 2. 插入文档
+	insertedID, err := insertDocument(collection)
+	if err != nil {
+		log.Println("插入失败:", err)
+	} else {
+		log.Println("插入成功，ID:", insertedID)
+	}
+
+	// 3. 查询文档
+	err = findDocuments(collection)
+	if err != nil {
+		log.Println("查询失败:", err)
+	}
+
+	// 4. 更新文档
+	err = updateDocument(collection, insertedID)
+	if err != nil {
+		log.Println("更新失败:", err)
+	}
+
+	// 5. 删除文档
+	err = deleteDocument(collection, insertedID)
+	if err != nil {
+		log.Println("删除失败:", err)
+	}
+}
+
+// 连接MongoDB
+func connectMongoDB() (*mongo.Client, error) {
+	// 设置客户端连接配置
+	clientOptions := options.Client().ApplyURI("mongodb://150.158.46.207:27018")
+	clientOptions.SetAuth(options.Credential{
+		Username: "root",
+		Password: "}aAW*Fd,G#TQu%2KUc~_",
+	})
+
+	// 连接到MongoDB
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	// 检查连接
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("成功连接到MongoDB")
+	return client, nil
+}
+
+// 插入文档
+func insertDocument(collection *mongo.Collection) (primitive.ObjectID, error) {
+	// 创建一个用户
+	user := User{
+		Name:     "张三",
+		Age:      30,
+		Email:    "zhangsan@example.com",
+		CreateAt: time.Now(),
+	}
+
+	// 插入单个文档
+	result, err := collection.InsertOne(context.TODO(), user)
+	if err != nil {
+		return primitive.ObjectID{}, err
+	}
+
+	// 获取插入的ID
+	insertedID := result.InsertedID.(primitive.ObjectID)
+	return insertedID, nil
+}
+
+// 批量插入文档
+func insertManyDocuments(collection *mongo.Collection) ([]primitive.ObjectID, error) {
+	// 创建多个用户
+	users := []interface{}{
+		User{Name: "李四", Age: 25, Email: "lisi@example.com", CreateAt: time.Now()},
+		User{Name: "王五", Age: 35, Email: "wangwu@example.com", CreateAt: time.Now()},
+	}
+
+	// 批量插入文档
+	result, err := collection.InsertMany(context.TODO(), users)
+	if err != nil {
+		return nil, err
+	}
+
+	// 转换为ObjectID切片
+	var ids []primitive.ObjectID
+	for _, id := range result.InsertedIDs {
+		ids = append(ids, id.(primitive.ObjectID))
+	}
+
+	return ids, nil
+}
+
+// 查询文档
+func findDocuments(collection *mongo.Collection) error {
+	// 1. 查询单个文档
+	var user User
+	err := collection.FindOne(context.TODO(), bson.D{{"name", "张三"}}).Decode(&user)
+	if err != nil {
+		return err
+	}
+	fmt.Println("查询到的单个文档:", user)
+
+	// 2. 查询多个文档
+	// 设置查询条件
+	filter := bson.D{{"age", bson.D{{"$gt", 20}}}} // 年龄大于20的
+
+	// 执行查询
+	cursor, err := collection.Find(context.TODO(), filter)
+	if err != nil {
+		return err
+	}
+	defer func(cursor *mongo.Cursor, ctx context.Context) {
+		err := cursor.Close(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(cursor, context.TODO())
+
+	// 遍历结果
+	var users []User
+	if err = cursor.All(context.TODO(), &users); err != nil {
+		return err
+	}
+
+	fmt.Println("查询到的多个文档:")
+	for _, u := range users {
+		fmt.Println(u)
+	}
+
+	return nil
+}
+
+// 更新文档
+func updateDocument(collection *mongo.Collection, id primitive.ObjectID) error {
+	// 设置查询条件，根据ID查询
+	filter := bson.D{{"_id", id}}
+
+	// 设置更新内容
+	update := bson.D{
+		{"$set", bson.D{
+			{"age", 31},
+			{"email", "updated@example.com"},
+		}},
+	}
+
+	// 执行更新
+	result, err := collection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("更新了 %v 个文档\n", result.ModifiedCount)
+	return nil
+}
+
+// 删除文档
+func deleteDocument(collection *mongo.Collection, id primitive.ObjectID) error {
+	// 设置删除条件
+	filter := bson.D{{"_id", id}}
+
+	// 执行删除
+	result, err := collection.DeleteOne(context.TODO(), filter)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("删除了 %v 个文档\n", result.DeletedCount)
+	return nil
+}
